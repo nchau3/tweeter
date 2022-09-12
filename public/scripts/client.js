9f6427efa24fd31c4e27a0120859b5d39af4f42e
Nicholas <nc_chau@hotmail.com>
2022-09-12 23:40:05 +0000

    implement function to load tweets from ajax GET request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,35 +6,10 @@
 
 $(document).ready(function () {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   const createTweetElement = function(tweetObj) {
     const { name, avatars, handle } = tweetObj.user;
     const tweet = tweetObj.content.text;
-    const datePosted = tweetObj.created_at;
+    const datePosted = timeago.format(tweetObj.created_at);
 
     return $(`
     <div class="container">
@@ -56,17 +31,29 @@ $(document).ready(function () {
           <i class="fa-solid fa-heart like-button"></i>
         </div>
       </footer>
-     </div>
+      </div>
     `);
-  }
+  };
 
   const renderTweets = function(tweets) {
     for (const entry in tweets) {
       const $tweet = createTweetElement(tweets[entry]);
       $('#tweet-container').append($tweet);
     }
-  }
+  };
   
- renderTweets(data);
-  
+  $('#tweet-form').submit(function(event) {
+    event.preventDefault();
+    $.post('/tweets', $(this).serialize());
+  });
+
+  const loadTweets = function() {
+    $.ajax('/tweets', {method: 'GET'})
+    .then(function (tweetsData) {
+      renderTweets(tweetsData);
+    })
+  };
+
+  loadTweets();
+
 });
