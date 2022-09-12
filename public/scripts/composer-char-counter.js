9b3447baceeca4b65cf98f2cd30b2c471b246598
Nicholas <nc_chau@hotmail.com>
2022-09-12 20:56:03 +0000

    implement char-counter function, style main tweet body

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $('#tweet-text').on('input', function () {
+    let count = $(this).val().length;
+    let counter = $(this).next().children()[1];
+    if (count > 140) {
+      $(counter).css('color', "red");
+      counter.innerHTML = 140 - count;
+    } else {
+      $(counter).css('color', "#545149");
+      counter.innerHTML = 140 - count;
+    }
+  })
+
+});
